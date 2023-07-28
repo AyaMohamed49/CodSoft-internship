179a2a0f1b7b8b8d1a4e06139cce508c33e16570
aya560 <yokam2849@gmail.com>
2023-07-28 03:01:31 +0300

    newProjectTagroba

--- Task1-FlashlightApp/app/src/main/java/com/example/task1_flashlightapp/MainActivity.java
+++ Task1-FlashlightApp/app/src/main/java/com/example/task1_flashlightapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.task1_flashlightapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageButton=findViewById(R.id.torchOffBtn);
+
+
     }
 }
